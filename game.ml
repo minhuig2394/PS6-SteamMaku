fbf9c2e503b81b9e007aa3c6d9ede4f2aea60f41
dw443 <dw443@users.noreply.github.com>
2013-11-29 18:16:51 -0500

    Update game.ml

--- game.ml
+++ game.ml
@@ -3,7 +3,9 @@ open Constants
 open Util
 
 type game = 
-    {redlife : int;
+    {redid : int;
+     blueid : int;
+     redlife : int;
      redscore : int;
      redcharge : int;
      redpos : position;
@@ -16,25 +18,27 @@ type game =
      redbullets : (bullet_type * position * velocity) list;
      bluebullets : (bullet_type * position * velocity) list;
      timer : int;
-     (*version 2 only
+     (*version 2 only*)
      redbombs : int;
      bluebombs : int;
      redinvinc : int;
      blueinvinc : int;
-     version 4 only
+     (*version 4 only*)
      redpower : int;
      bluepower : int;
      ufotimer : int;
      numufo : int;
      numpower : int;
      ufopos : position list;
-     powerpos : power list;*)
+     powerpos : power list;
    }
 
 
 let init_game () : game =
   let init : game = 
-    {init.redlife = cINITIAL_LIVES;
+    {init.redid = 1;
+     init.blueid = 2;
+     init.redlife = cINITIAL_LIVES;
      init.redscore = 0;
      init.redcharge = 0;
      init.redpos = 
@@ -51,19 +55,19 @@ let init_game () : game =
      init.redbullets = [];
      init.bluebullets = [];
      init.timer = cTIME_LIMIT;
-     (*version 2 only
+     (*version 2 only*)
      init.redbombs = cINITIAL_BOMBS;
      init.bluebombs = cINITIAL_BOMBS;
      init.redinvinc = cBOMB_DURATION;
      init.blueinvinc = cBOMB_DURATION;
-     version 4 only
+     (*version 4 only*)
      init.redpower = 0;
      init.bluepower = 0;
      init.ufotimer : int;
      init.numufo = 0;
      init.numpower = 0;
-     init.ufopos : position list;
-     init.powerpos : position list;*)
+     init.ufopos = [];
+     init.powerpos = [];
    } in
   init
 
@@ -74,4 +78,34 @@ let handle_action game col act =
   failwith "A myon sort of day"
 
 let get_data game =
-  failwith "I'm the strongest!"
+  let redp : player_char = 
+    (p_id = game.redid; 
+     p_pos = game.redpos; 
+     p_focused = game.redfoc;
+     p_radius = cHITBOX_RADIUS;
+     p_color = RED;} in
+  let bluep : player_char = 
+    {p_id = game.blueid; 
+     p_pos = game.bluepos; 
+     p_focused = game.bluefoc;
+     p_radius = cHITBOX_RADIUS;
+     p_color = BLUE;} in
+  let red : team_data = 
+  (game.redlife, 
+   game.redbombs, 
+   game.redscore, 
+   game.redpower, 
+   game.redcharge, 
+   redp) in
+  let blue : team_data = 
+  (game.bluelife, 
+   game.bluebombs, 
+   game.bluescore, 
+   game.bluepower, 
+   game.bluecharge, 
+   bluep) in
+  let ufos : ufo list = [] in (*temporary until implemented*)
+  let bullets : bullet list = [] in 
+  let powers : power list = [] in (*temporary until implemented*)
+  let data : game_data = 
+    red * blue * ufos * bullets * powers
