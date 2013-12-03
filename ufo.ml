0de9816c201cd3cebaffc3972c1796cc86ffb520
minhuig2394 <minhuig2394@gmail.com>
2013-12-03 00:30:37 -0500

    update ufo.ml;  new**design overview
    updated: functions in ufo.ml updated
    started: design overview document

--- ufo.ml
+++ ufo.ml
@@ -8,43 +8,65 @@ type ufo = {
 }
 
 
-let random_pradius radius =
-
-let create_power id n radius pcolor = 
-	let rec create n id plst= 
-	if n > 0 then 
-		let p = 
-		{b_type = Power 
-		b_id = id;
-  		b_pos = random_pradius;
-  		b_vel = velocity;
-  		b_accel = (0.,0.);
-  		b_radius cPOWER_RADIUS;
-  		b_color = pcolor
-		} in 
-		create (n - 1) (id + 1) (p::plst)
-  else plst
-  in create n id []
-
 let init_vel speed target pos= 
-	let sp = float_of_int speed in 
-	let x = ((fst target) -. (fst pos)) in
+  let sp = float_of_int speed in 
+  let x = ((fst target) -. (fst pos)) in
   let y = ((snd target) -. (snd pos)) in 
   let hyp = sqrt (x*.x +. y*.y) in 
   let sine = y/.hyp in 
   let cosine = x/.hyp in 
   (sp*.cosine,sp*.sine)
 
+let random_position = 
+  let onef = 1./.4. *. (float_of_int cBOARD_WIDTH) in  
+  let threef = 3./.4. *. (float_of_int cBOARD_WIDTH) in 
+  let bound = threef -. onef in 
+  let randx = Random.float bound +. onef in 
+  let halfy= (float_of_int (cBOARD_HEIGHT/2)) in 
+  let flip = Random.int 2 in 
+    let randy = Random.float halfy in 
+    if flip = 1 then 
+      (randx,randy+.halfy)
+    else 
+      (randx, randy)
 
-let random_position  = 
+let roll x= let r = Random.int 2 in 
+  if r = 0 then x
+  else ~-.x
 
-let random_direction = 
+let random_pradius radius center =
+  let randx = Random.float (float_of_int radius) in 
+  let randy = Random.float (float_of_int radius) in 
+    ((roll randx)+.(fst center)), ((roll randy)+.(snd center))
+
+let create_power (id:int) (u:ufo) (rpos:position) (bpos:position)= 
+  let reds = h/u.u_red_hits in 
+	let rec create n r id plst= 
+	if n > 0 then 
+    let pos = random_pradius radius u.u_pos in 
+		let p = 
+		{b_type = Power 
+		b_id = id;
+  		b_pos = pos;
+  		b_vel = if r < reds then (init_vel cPOWER_SPEED rpos pos)
+              else (init_vel cPOWER_SPEED bpos pos)
+  		b_accel = (0.,0.);
+  		b_radius cPOWER_RADIUS;
+  		b_color = pcolor
+		} in 
+		create (n - 1) (r + 1) (id + 1) (p::plst)
+  else plst
+  in create cUFO_POWER_NUM 0 id []
+
+let random_vel upos= 
+  let r = random_position in 
+  init_vel cUFO_SPEED r upos
 
 let create_ufo id= 
 let rdir = random_direction
 	{u_id = id;
   	u_pos = rdir;
-  	u_vel = init_vel cUFO_SPEED rdir random_position 
+  	u_vel = random_vel rdir
   	u_radius = cUFO_RADIUS;
   	u_red_hits = 0;
  	 u_blue_hits = 0
