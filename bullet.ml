18f4086b35a40a598b20ccb904716bab38502bbe
minhuig2394 <minhuig2394@gmail.com>
2013-12-03 16:45:55 -0500

    update bullet.ml, ufo.ml

--- bullet.ml
+++ bullet.ml
@@ -4,26 +4,20 @@ open Game
 open Util 
 open Player 
 
-let add_vector a b = 
-	((fst a) +. (fst b)),((snd a) +. (snd b))
-let sub_vector a b = 
-	((fst a) -. (fst b)),((snd a) -. (snd b))
-
 (*determines if two circles intersect*)
 let collide (point:position) (r1:int) (center:position) (r2:int) = 
   let radius = float_of_int (r1 + r2) in 
-  let diff = sub_vector point center in 
+  let diff = subt_v point center in 
   ((fst diff)*.(fst diff) +. (snd diff)*.(snd diff)) < radius*.radius 
 
 let set_pos (b:bullet) = 
 	{b with 
-		b_pos = add_vector b.b_pos b.b_vel;
-		b_vel = add_vector b.b_vel b.b_accel}
+		b_pos = add_v b.b_pos b.b_vel;
+		b_vel = add_v b.b_vel b.b_accel}
 
 (*determines if a bullet is out of bounds*)
 let out (b:bullet) = 
-  b.b_pos <= (float_of_int cBOARD_WIDTH,float_of_int cBOARD_HEIGHT) 
-
+  not (in_bounds b.b_pos)
 (*determines if a bullet's radius and a player's radius intersect*)
 let hit (b:bullet) (p:player_char) = 
   collide b.b_pos b.b_radius p.p_pos p.p_radius
@@ -56,37 +50,31 @@ let init_vel speed target pos=
 	let sp = float_of_int speed in 
 	let x = ((fst target) -. (fst pos)) in
   let y = ((snd target) -. (snd pos)) in 
-  let hyp = sqrt (x*.x +. y*.y) in 
-  let sine = y/.hyp in 
-  let cosine = x/.hyp in 
+  let cosine,sine = unit_v (x,y) in 
   (sp*.cosine,sp*.sine)
 
-let create_spread id target plpos pcolor acc = 
-  let x = ((fst target) -. (fst plpos)) in
-  let y = ((snd target) -. (snd plpos)) in 
-  let or_angle = atan2 y x in
-  let sp = float_of_int cSPREAD_SPEED in 
-  let pi = 4.0 *. atan 1.0 in 
-  let ang = (float_of_int (360/cSPREAD_NUM))*.(pi /. 180.) in 
-	let rec screate n angle id lst = 
+let create_spread target plpos pcolor acc = 
+  let ang = deg_to_rad (float_of_int (360/cSPREAD_NUM)) in 
+  let initial_velocity = init_vel cSPREAD_SPEED target plpos in
+	let rec screate n angle lst = 
     let new_ang = angle+.ang in 
-		if not (n = 0) then 
+		if n > 0 then 
 			let s = {b_type = Spread;
-				b_id = id;
+				b_id = next_available_id ();
 				b_pos = plpos;
-				b_vel = sp*.(cos new_ang),sp*.(sin new_ang);
+				b_vel = rotate initial_velocity new_ang;
 				b_accel = (if fst(acc) <= cACCEL_LIMIT && fst(acc) <= cACCEL_LIMIT then 
           acc else (0.,0.));
 				b_radius = cSPREAD_RADIUS;
 				b_color = pcolor} in 
 				let new_bullst = s::lst
-			in screate (n - 1) new_ang (id + 1) new_bullst 
+			in screate (n - 1) new_ang new_bullst 
 		else lst 
-	in screate cSPREAD_NUM or_angle id [] 
+	in screate cSPREAD_NUM 0.0 [] 
 
-let create_bubble id target plpos pcolor acc= 
+let create_bubble  target plpos pcolor acc= 
 	let b = {b_type = Bubble;
-				b_id = id;
+				b_id = next_available_id ();
 				b_pos = plpos;
 				b_vel = init_vel cBUBBLE_SPEED target plpos;
 				b_accel = (if fst(acc) <= cACCEL_LIMIT && fst(acc) <= cACCEL_LIMIT then 
@@ -95,50 +83,34 @@ let create_bubble id target plpos pcolor acc=
 				b_color = pcolor} in 
 				[b]
 
-let create_trail id plpos pcolor target acc= 
-  let ang = (float_of_int cTRAIL_ANGLE)*.(pi /. 180.) in 
-  let x = ((fst target) -. (fst plpos)) in
-  let y = ((snd target) -. (snd plpos)) in 
-  let sp = float_of_int cTRAIL_SPEED_STEP in 
-  let angle = atan2 y x in
+let create_trail plpos pcolor target acc= 
+  let ang = deg_to_rad (float_of_int cTRAIL_ANGLE) in 
   let rec trail n lst= 
-  if n > 0 then 
-		let t = {b_type = Trail;
-			b_id = id;
-			b_pos = plpos;
-			b_vel = (sp*.(cos angle),sp*.(sin angle));
-			b_accel = (if fst(acc) <= cACCEL_LIMIT && fst(acc) <= cACCEL_LIMIT then 
+    if n > 0 then 
+    let rec trail_helper n angle lst = 
+      if n > 0 then 
+        let t = 
+          {b_type = Trail;
+          b_id = next_available_id ();
+          b_pos = plpos;
+          b_vel = 
+            (let next_vel = (init_vel (cTRAIL_SPEED_STEP*n) target plpos) in 
+            rotate next_vel angle); 
+          b_accel = (if fst(acc) <= cACCEL_LIMIT && fst(acc) <= cACCEL_LIMIT then 
           acc else (0.,0.));
-			b_radius = cTRAIL_RADIUS;
-			b_color = pcolor} in 
-		  let nlst = t::lst
-    in let t2 = 
-      {b_type = Trail;
-        b_id = id +1;
-        b_pos = plpos;
-        b_vel =  ((sp+.sp)*.(cos (angle+.ang)),(sp+.sp)*.(sin (angle+.ang)));
-        b_accel = (if fst(acc) <= cACCEL_LIMIT && fst(acc) <= cACCEL_LIMIT then 
-          acc else (0.,0.));
-        b_radius = cTRAIL_RADIUS;
-        b_color = pcolor} in 
-       let mlst = t2::nlst
-    in let t3 = 
-      {b_type = Trail;
-        b_id = id +2;
-        b_pos = plpos;
-        b_vel =  ((sp+.sp+.sp)*.(cos (angle-.ang)),(sp+.sp+.sp)*.(sin (angle-.ang)));
-        b_accel = (if fst(acc) <= cACCEL_LIMIT && fst(acc) <= cACCEL_LIMIT then 
-          acc else (0.,0.));
-        b_radius = cTRAIL_RADIUS;
-        b_color = pcolor} in 
-      let rlst = t3::mlst in 
-      trail (n - 1) rlst
-			else lst 
-    in trail cTRAIL_NUM  []
+          b_radius = cTRAIL_RADIUS;
+          b_color = pcolor} in 
+          trail_helper (n-1) (angle-.ang) (t::lst)
+      else lst 
+      in let newlst = trail_helper 3 ang lst 
+      in trail (n - 1) newlst
+    else lst 
+  in trail cTRAIL_NUM []
 
-let create_bullet b target id plpos pcolor acc= 
+let create_bullet b target plpos pcolor acc= 
 	match b with 
-	|Spread -> create_spread id target plpos pcolor acc 
-	|Bubble -> create_spread id target plpos pcolor acc  
-	|Trail ->  create_trail  id plpos pcolor target acc
+	|Spread -> create_spread  target plpos pcolor acc 
+	|Bubble -> create_spread  target plpos pcolor acc  
+	|Trail ->  create_trail  plpos pcolor target acc
+  |Power -> failwith "Error: Power not a bullet"
 
--- ufo.ml
+++ ufo.ml
@@ -7,17 +7,14 @@ type ufo = {
   u_blue_hits : int
 }
 
-
 let init_vel speed target pos= 
   let sp = float_of_int speed in 
   let x = ((fst target) -. (fst pos)) in
   let y = ((snd target) -. (snd pos)) in 
-  let hyp = sqrt (x*.x +. y*.y) in 
-  let sine = y/.hyp in 
-  let cosine = x/.hyp in 
+  let cosine,sine = unit_v (x,y) in 
   (sp*.cosine,sp*.sine)
 
-let random_position = 
+let random_position =
   let onef = 1./.4. *. (float_of_int cBOARD_WIDTH) in  
   let threef = 3./.4. *. (float_of_int cBOARD_WIDTH) in 
   let bound = threef -. onef in 
@@ -30,47 +27,45 @@ let random_position =
     else 
       (randx, randy)
 
-let roll x= let r = Random.int 2 in 
-  if r = 0 then x
-  else ~-.x
-
 let random_pradius radius center =
   let randx = Random.float (float_of_int radius) in 
   let randy = Random.float (float_of_int radius) in 
     ((roll randx)+.(fst center)), ((roll randy)+.(snd center))
 
-let create_power (id:int) (u:ufo) (rpos:position) (bpos:position)= 
-  let reds = h/u.u_red_hits in 
-	let rec create n r id plst= 
-	if n > 0 then 
-    let pos = random_pradius radius u.u_pos in 
-		let p = 
-		{b_type = Power 
-		b_id = id;
-  		b_pos = pos;
-  		b_vel = if r < reds then (init_vel cPOWER_SPEED rpos pos)
-              else (init_vel cPOWER_SPEED bpos pos)
-  		b_accel = (0.,0.);
-  		b_radius cPOWER_RADIUS;
-  		b_color = pcolor
-		} in 
-		create (n - 1) (r + 1) (id + 1) (p::plst)
-  else plst
-  in create cUFO_POWER_NUM 0 id []
 
 let random_vel upos= 
   let r = random_position in 
   init_vel cUFO_SPEED r upos
 
-let create_ufo id= 
-let rdir = random_direction
-	{u_id = id;
-  	u_pos = rdir;
-  	u_vel = random_vel rdir
-  	u_radius = cUFO_RADIUS;
-  	u_red_hits = 0;
- 	 u_blue_hits = 0
-	}
+let create_ufo = 
+let rdir = random_position in 
+  {u_id = next_available_id ();
+    u_pos = rdir;
+    u_vel = random_vel rdir;
+    u_radius = cUFO_RADIUS;
+    u_red_hits = 0;
+   u_blue_hits = 0
+  }
+
+let create_power  (u:ufo) (rpos:position) (bpos:position)= 
+  let h = (u.u_red_hits + u.u_blue_hits) in 
+  let reds = (float u.u_red_hits/. float h)*.(float cUFO_POWER_NUM) in 
+	let rec create n r plst= 
+	if n > 0 then 
+    let pos = random_pradius cUFO_SCATTER_RADIUS u.u_pos in 
+		let p = 
+		  {b_type = Power;
+		  b_id = next_available_id ();
+  		b_pos = pos;
+  		b_vel = if r < reds then (init_vel cPOWER_SPEED rpos pos)
+        else (init_vel cPOWER_SPEED bpos pos);
+  		b_accel = (0.,0.);
+  		b_radius = cPOWER_RADIUS;
+  		b_color = if r < reds then Red else Blue;
+		} in 
+		create (n - 1) (r +. 1.) (p::plst)
+  else plst
+  in create cUFO_POWER_NUM 0.0 []
 
 let update_ufo ufolst= 
 	let rec update ulst rlst = 
