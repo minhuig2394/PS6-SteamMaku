2a1e97b7ebfa827abee8192592696ff76cdeb87d
dw443 <dw443@users.noreply.github.com>
2013-12-04 11:38:00 -0500

    Update game.ml

--- game.ml
+++ game.ml
@@ -1,6 +1,7 @@
 open Definitions
 open Constants
 open Util
+open Netgraphics
 
 type game = 
     {redid : int;
@@ -15,8 +16,8 @@ type game =
      bluecharge : int;
      bluepos : position;
      bluefoc : bool;
-     redbullets : (bullet_type * position * velocity) list;
-     bluebullets : (bullet_type * position * velocity) list;
+     redbullets : bullet list;
+     bluebullets : bullet list;
      timer : int;
      (*version 2 only*)
      redbombs : int;
@@ -27,101 +28,114 @@ type game =
      redpower : int;
      bluepower : int;
      ufotimer : int;
-     numufo : int;
      numpower : int;
-     ufopos : position list;
-     powerpos : power list;
+     ufos : ufo list;
+     powers : power list;
    }
 
 
 let init_game () : game =
   let init : game = 
-    {init.redid = 1;
-     init.blueid = 2;
-     init.redlife = cINITIAL_LIVES;
-     init.redscore = 0;
-     init.redcharge = 0;
-     init.redpos = 
+    {redid = 1;
+     blueid = 2;
+     redlife = cINITIAL_LIVES;
+     redscore = 0;
+     redcharge = 0;
+     redpos = 
      ((float_of_int c_BOARD_WIDTH) /. 8., 
       (float_of_int cBOARD_HEIGHT) /. 2.);
-     init.redfoc = false;
-     init.bluelife = cINITIAL_LIVES;
-     init.bluescore = 0;
-     init.bluecharge = 0;
-     init.bluepos =
+     redfoc = false;
+     bluelife = cINITIAL_LIVES;
+     bluescore = 0;
+     bluecharge = 0;
+     bluepos =
      (((float_of_int c_BOARD_WIDTH) /. 8.) *. 7., 
       (float_of_int cBOARD_HEIGHT) /. 2.)
-     init.bluefoc = false;
-     init.redbullets = [];
-     init.bluebullets = [];
-     init.timer = cTIME_LIMIT;
+     bluefoc = false;
+     redbullets = [];
+     bluebullets = [];
+     timer = cTIME_LIMIT;
      (*version 2 only*)
-     init.redbombs = cINITIAL_BOMBS;
-     init.bluebombs = cINITIAL_BOMBS;
-     init.redinvinc = cBOMB_DURATION;
-     init.blueinvinc = cBOMB_DURATION;
+     redbombs = cINITIAL_BOMBS;
+     bluebombs = cINITIAL_BOMBS;
+     redinvinc = cBOMB_DURATION;
+     blueinvinc = cBOMB_DURATION;
      (*version 4 only*)
-     init.redpower = 0;
-     init.bluepower = 0;
-     init.ufotimer : int;
-     init.numufo = 0;
-     init.numpower = 0;
-     init.ufopos = [];
-     init.powerpos = [];
+     redpower = 0;
+     bluepower = 0;
+     ufotimer : int;
+     numpower = 0;
+     ufos = [];
+     powers = [];
    } in
+  add_update (AddPlayer (init.redid, Red, init.redpos));
+  add_update (AddPlayer (init.blueid, Blue, init.bluepos));
+  add_update (SetBomb (Red, init.redbombs));
+  add_update (SetBomb (Blue, init.bluebombs));
+  add_update (SetLives (Red, init.redlife));
+  add_update (SetLives (Blue, init.bluelife));
+  add_update (SetScore (Red, init.redscore));
+  add_update (SetScore (Blue, init.bluescore));
+  add_update (SetPower (Red, init.redpower));
+  add_update (SetPower (Blue, init.bluepower));
+  add_update (SetCharge (Red, init.redcharge));
+  add_update (SetCharge (Blue, init.bluecharge));
   init
 
 let handle_time game =
-  failwith "Charisma BREAK"
+  (*check if red player has no more life
+    check if blue player has no more life
+    check if red or blue player has more points if no more time*)
+  failwith "not implemented"
 
 let handle_action game col act =
-   match col, act with
-    |Blue, Move _  -> game
-    |Red, Move _ -> game
-    |Blue, Shoot b -> game
-    |Red, Shoot b -> game
-    |Blue, Focus x ->
-       let updated = {
-        game with 
-        redfocus = x
-        } in updated
-    |Red, Focus x -> 
-       let updated = {
-        game with 
-        redfocus = x
-        } in updated
+  match col, act with
+  |Blue, Move _ -> game
+  |Red, Move _ -> game
+  |Blue, Shoot (b, p, a) -> game
+  |Red, Shoot (b, p, a)-> game
+  |Blue, Focus x ->
+      let updated = 
+	{game with 
+	 redfocus = x
+       } in updated
+  |Red, Focus x -> 
+      let updated = 
+	{game with 
+	 redfocus = x
+       } in updated
   |Blue, Bomb -> 
-    let updated = {
-    game with 
-    redbullets = []; 
-    bluebullets = [];
-    blueinvinc = cBomb_Duration;
-    bluebombs = game.bluebombs - 1
-    } in updated
+      let updated = 
+	{game with 
+	 redbullets = []; 
+	 bluebullets = [];
+	 blueinvinc = cBomb_Duration;
+	 bluebombs = game.bluebombs - 1
+       } in 
+  add_update (UseBomb (Blue));
+  updated
   |Red, Bomb -> 
-    let updated = {
-    game with 
-    redbullets = []; 
-    bluebullets = [];
-    redinvinc = cBomb_Duration;
-    redbombs = game.redbombs - 1
-    } in updated
+      let updated = 
+	{game with 
+	 redbullets = []; 
+	 bluebullets = [];
+	 redinvinc = cBomb_Duration;
+	 redbombs = game.redbombs - 1
+       } in 
+  add_update (UseBomb (Red));
+  updated
 (*action -> Move of (direction * direction) list
             Shoot of (bullet_type * position * acceleration
             bullet_type can be Spread | Bubble | Power | Trail*)
 
-(*For Move: 
+(*For Move: Need to use MovePlayer (id, pos) graphic update
 
   For Shoot: Deplete Charge
   Change bullet list
 
-  For Focus: Change the focus of the player
-
-  For Bomb: clear the screen of bullets, 
-  make the user invincible
+  For Bomb:    
   Remove any bullets that the invincible user grazes during duration of bomb
   No charge accumulated
-  Subtract number of bombs available to the player
 *)
 
 
@@ -152,8 +166,7 @@ let get_data game =
    game.bluepower, 
    game.bluecharge, 
    bluep) in
-  let ufos : ufo list = [] in (*temporary until implemented*)
-  let bullets : bullet list = [] in 
-  let powers : power list = [] in (*temporary until implemented*)
+  let totalbullets : bullet list = 
+    List.append redbullets bluebullets in
   let data : game_data = 
-    red * blue * ufos * bullets * powers
+    red * blue * game.ufos * totalbullets * game.powers
