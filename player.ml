ccb50a19f57b7d42f6e0b6b76179368c70176aed
dw443 <dw443@users.noreply.github.com>
2013-12-05 01:07:02 -0500

    Update player.ml

--- player.ml
+++ player.ml
@@ -1,18 +1,11 @@
 open Definitions
 open Constants
-open Game
-open Util 
-open Bullet 
+open Util
+open Bullet
 
-let  = 
-
-lives 
-bombs 
-score
-power
-charge 
-
-focus 
+let move pos direction speed = 
+  let x = vector_of_dirs direction (float_of_int speed) in
+  add_v pos x
 
 
 
