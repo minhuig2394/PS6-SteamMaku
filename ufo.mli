fa9df46914ef69ae46aec9c3eacef85fede46e8c
minhuig2394 <minhuig2394@gmail.com>
2013-12-05 17:49:37 -0500

    update ufo.mli

--- ufo.mli
+++ ufo.mli
@@ -1,3 +1,4 @@
+val hit_ufo : Definitions.bullet -> Definitions.ufo -> bool
 (*create_power u bpos rpos takes in a ufo, 
 *the position of the red player and the position of 
 *the blue player and creates and returns a list of power ups*)
