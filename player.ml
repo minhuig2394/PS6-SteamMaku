00da0be796629b45d6b9d650428ec49b357ae91f
dw443 <dw443@users.noreply.github.com>
2013-12-05 01:31:45 -0500

    Update player.ml

--- player.ml
+++ player.ml
@@ -3,9 +3,10 @@ open Constants
 open Util
 open Bullet
 
-let move pos direction speed = 
-  let x = vector_of_dirs direction (float_of_int speed) in
+let move pos direction foc = 
+  if foc = true then
+  let x = vector_of_dirs direction (float_of_int cFOCUSED_SPEED) in
   add_v pos x
-
-
-
+  else
+    let y = vector_of_dirs direction (float_of_int cUNFOCUSED_SPEED) in
+    add_v pos y
