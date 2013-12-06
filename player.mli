ea9ce3f863f054b966746280b524058b955bdf69
dw443 <dw443@users.noreply.github.com>
2013-12-06 00:03:09 -0500

    Update player.mli

--- player.mli
+++ player.mli
@@ -1,13 +1,22 @@
 (*move takes in a position, direction, and speed (focused or unfocused) 
   and returns a position, which allows us to update the position
   of the player*)
-
 val move_player: Definitions.position -> 
   (Definitions.direction * Definitions.direction) -> bool -> 
     Definitions.position
-  
+
 val g_result: int -> int -> int -> int -> float -> Definitions.result
 
 val shoot: Definitions.bullet_type -> Definitions.position -> 
   Definitions.position -> Definitions.acceleration -> int 
     -> Definitions.color -> (Definitions.bullet list * int)
+
+val newlife: int -> bool -> int
+
+val newscore: bool -> int -> int -> int -> int
+
+val newpower: int -> bool -> int -> int
+
+val newinvinc: bool -> int -> int
+
+val charge: int -> int -> bool -> int -> int
