30c1609a91dceb72d6688881195c3e0140daf95e
dw443 <dw443@users.noreply.github.com>
2013-12-06 17:41:33 -0500

    Update game.ml

--- game.ml
+++ game.ml
@@ -118,7 +118,7 @@ let handle_time game =
     {red = redp;
      blue = bluep;
      rinvincible = if game.redinvinc - 1 = 0 then false else true;
-     binvincible = game.blueinvinc - 1 = 0 then false else true;
+     binvincible = if game.blueinvinc - 1 = 0 then false else true;
      blst = game.bullets;
      ufolst = game.ufos;
      pwrlst = game.powers;
