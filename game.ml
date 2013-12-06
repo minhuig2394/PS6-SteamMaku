9f7d38e7154df28cafbd9afb2dbe6ba6cd1df0dd
dw443 <dw443@users.noreply.github.com>
2013-12-06 17:20:18 -0500

    Update game.ml

--- game.ml
+++ game.ml
@@ -196,7 +196,7 @@ let handle_time game =
   add_update (SetPower (Blue, updated.bluepower));
   add_update (SetCharge (Red, updated.redcharge));
   add_update (SetCharge (Blue, updated.bluecharge));
-  add_update (CountDown (updated.timer));
+  add_update (Countdown (updated.timer));
   let r = (g_result updated.redlife updated.bluelife updated.redscore
       updated.bluescore updated.timer) in
   match r with
